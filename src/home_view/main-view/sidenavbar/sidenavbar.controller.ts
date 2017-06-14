dad332b4e48c5533d94769527fb58e810b49e628
aashish347 <aashish.kumar@elucidata.io>
2017-06-14 17:55:00 +0530

    add buttons in accordion in sidenavbar add styles

--- src/home_view/main-view/sidenavbar/sidenavbar.controller.ts
+++ src/home_view/main-view/sidenavbar/sidenavbar.controller.ts
@@ -2,14 +2,28 @@
 import Inject from "../../../utils/di"
 export default class SideNavBarController {
   private style = require('./sidenavbar.css');
-  
-  constructor(@Inject('$scope') private $scope: any) {
+  private states = [
+    'main.introductionPage',
+    'main.gettingStarted',
+    'main.progressbuttonDemo',
+    'main.filevalidationlogDemo',
+    'main.progressButton',
+    'main.fileTabs'
+  ];
+
+  constructor(@Inject('$state') private $state: any) {
     
   }
 
   
+  /**
+   * @param  {number} id
+   * @returns void
+   * changes state according to id
+   */
+  public changeState(id: number): void {
+    this.$state.go(this.states[id]);
+  }
 
-
-  
 
 }
