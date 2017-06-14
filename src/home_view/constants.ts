e804855d5b817e7bf0a6a1499c8d3999990fd0a3
aashish347 <aashish.kumar@elucidata.io>
2017-06-14 20:11:22 +0530

    make constants.ts for storing constant values

---
+++ src/home_view/constants.ts
@@ -0,0 +1,21 @@
+
+export const INITIAL_STATE_OBJECT = {
+    'value': 'initial',
+    'text': 'Upload',
+    'color': '#337ab7'
+};
+export const PROGRESS_STATE_OBJECT = {
+    'value': 'inprogress',
+    'text': 'Uploading',
+    'color': '#a9a9a9'
+};
+export const DONE_STATE_OBJECT = {
+    'value': 'done',
+    'text': 'done',
+    'color': '#5cb85c'
+};
+export const ERROR_STATE_OBJECT = {
+    'value': 'error',
+    'text': 'error',
+    'color': '#d9534f'
+};
--- src/home_view/demo/progressButton/progressButtonDemo.controller.ts
+++ src/home_view/demo/progressButton/progressButtonDemo.controller.ts
@@ -1,37 +1,18 @@
 
-
+import * as CONSTANTS from '../../constants';
 export default class ProgressButtonDemoController {
-    
+
   private style = require('./progressButtonDemo.css');
   private icons: string[] = ['publish','check_circle','error','file_upload'];
   private isDisabled = false;
   private state: number = 0;
   private iconNo: number = 0;
-  private initialStateObject = {
-    'value':'initial',
-    'text':'Upload',
-    'color':'#337ab7'
-  };
-  private progressStateObject = {
-    'value':'inprogress',
-    'text':'Uploading',
-    'color':'#a9a9a9'
-  };
-  private doneStateObject = {
-    'value':'done',
-    'text':'done',
-    'color':'#5cb85c'
-  };
-  private errorStateObject = {
-    'value':'error',
-    'text':'error',
-    'color':'#d9534f'
-  };
+  
   private stateObject  = [
-    this.initialStateObject,
-    this.doneStateObject,
-    this.errorStateObject,
-    this.progressStateObject
+    CONSTANTS.INITIAL_STATE_OBJECT,
+    CONSTANTS.DONE_STATE_OBJECT,
+    CONSTANTS.ERROR_STATE_OBJECT,
+    CONSTANTS.PROGRESS_STATE_OBJECT
   ];
   constructor() {
     
