749e2d8a124391ea18e026f99c0851abac490815
aashish347 <aashish.kumar@elucidata.io>
2017-06-14 13:42:08 +0530

    register toolbar directive to module

--- src/home_view/appViewDirective.ts
+++ src/home_view/appViewDirective.ts
@@ -1,6 +1,7 @@
 
 import MainView from './main-view/mainView/mainview.directive';
-import SideNavBar from './main-view/sidenavbar/sidenavbar.directive'
+import SideNavBar from './main-view/sidenavbar/sidenavbar.directive';
+import Toolbar from './main-view/toolbar/toolbar.directive';
 
 export default function registerAppDirective(AppName: string): void {
 
@@ -10,5 +11,6 @@ export default function registerAppDirective(AppName: string): void {
         .module(AppName)
         .directive('mainView', MainView.Factory())
         .directive('sidenavbar',SideNavBar.Factory())
+        .directive('toolbar',Toolbar.Factory())
 
 }
