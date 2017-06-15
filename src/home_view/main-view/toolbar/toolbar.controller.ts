974f5322b9659e76cfebe65b7482c65e147e8ed9
aashish347 <aashish.kumar@elucidata.io>
2017-06-15 11:00:03 +0530

    show toolbar heading according to different states

--- src/home_view/main-view/toolbar/toolbar.controller.ts
+++ src/home_view/main-view/toolbar/toolbar.controller.ts
@@ -1,13 +1,23 @@
 
-import Inject from "../../../utils/di"
+import Inject from "../../../utils/di";
+import {STATES, TOOLBAR_STATES} from '../../constants';
+
 export default class ToolbarController {
   private style = require('./toolbar.css');
   
-  constructor(@Inject('$scope') private $scope: any) {
+  constructor(@Inject('$state') private $state: any) {
     
   }
 
-  
+  private get toolbarHeading(): string[] {
+    let heading: string[] = [''];
+    for(let state in STATES){
+      if(this.$state.current.name == STATES[state]){
+        return TOOLBAR_STATES[state];
+      }
+    }
+    return heading;
+  }
 
 
   
