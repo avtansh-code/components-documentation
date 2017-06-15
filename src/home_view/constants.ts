77a02867a8c090a44dc20a2870031b9f835b463d
aashish347 <aashish.kumar@elucidata.io>
2017-06-15 21:43:06 +0530

    add demo for file validation logs

--- src/home_view/constants.ts
+++ src/home_view/constants.ts
@@ -40,7 +40,7 @@ export const TOOLBAR_STATES =[
     ],
     [
         'Demo',
-        'file Validation Logs'
+        'File Validation Logs'
     ],
     [
         'Directive',
@@ -50,4 +50,31 @@ export const TOOLBAR_STATES =[
         'Directive',
         'file-tabs'
     ],
-]
+];
+
+export const FILE_VALIDATION_LOGS = {
+    "file 1": {
+        "errors": ["error1", "error2", "error3", "error4", "error5", "error6"],
+        "warnings": []
+    },
+    "file 2": {
+        "errors": ["error1", "error2", "error3", "error4"],
+        "warnings": ["warning1", "warning2", "warning3", "warning4"]
+    },
+    "file 3": {
+        "errors": ["error1", "error2", "error3"],
+        "warnings": ["warning1", "warning2", "warning3", "warning4", "warning5", "warning6"]
+    },
+    "file 4": {
+        "errors": [],
+        "warnings": [],
+    },
+    "file 5": {
+        "errors": [],
+        "warnings": ["warning1", "warning2", "warning3", "warning4", "warning5"]
+    },
+    "file 6": {
+        "errors": ["error1"],
+        "warnings": ["warning1"]
+    }
+}
