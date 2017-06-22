0a6ad7854de1e0eea54a8f7c6f4f859e14bf1fae
aashish347 <aashish.kumar@elucidata.io>
2017-06-22 18:21:34 +0530

    add search-select-dropdown-directive description directive

---
+++ src/home_view/directive_description/searchSelectDropdown/searchSelectDropdownDescription.controller.ts
@@ -0,0 +1,13 @@
+
+
+export default class SearchSelectDropdownDescriptionController {
+    
+  private style = require('./searchSelectDropdownDescription.css');
+  
+  constructor() {
+    
+  }
+
+  
+
+}
