42bc24700fbf36abe55c3bea70dfca92c4b82b5a
aashish347 <aashish.kumar@elucidata.io>
2017-07-17 20:21:35 +0530

    use component attribute table for showing attribute description of directives

--- src/home_view/directive_description/progressButton/progressButtonDescription.controller.ts
+++ src/home_view/directive_description/progressButton/progressButtonDescription.controller.ts
@@ -1,9 +1,36 @@
 
+import {IfcAttribute} from '../../../dataType';
 
 export default class ProgressButtonDescriptionController {
     
   private style = require('./progressButtonDescription.css');
-  
+  private attribute: IfcAttribute[] =[
+    {
+      parameter: 'state',
+      type: 'Json object',
+      description: `model to define state of the button with text to be displayed and its 
+                    background color.It contains key/value pairs.It contains following keys -
+                      <ul >
+                          <li>value  </li>
+                          <li>text  &nbsp &nbsp &nbsp<code>optional</code></li>
+                          <li>color  &nbsp &nbsp<code>optional</code></li>
+                      </ul>
+                    `
+    },
+    {
+      parameter: 'disabledstate',
+      type: 'boolean',
+      description: `Determines whether or not to disable the progress-button`
+    },
+    {
+      parameter: 'icon',
+      type: 'string',
+      description: `String name of icon for initial state. icons should be from 
+                      <a href="https://klarsys.github.io/angular-material-icons/">
+                      angular-material-icons</a> only
+                    `
+    }
+  ];
   constructor() {
     
   }
